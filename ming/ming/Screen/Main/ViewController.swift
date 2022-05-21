c6f7b62a65b76b31d1b0dc5eca32dfd6e2c7fe32
yoonseo <yo26on@soongsil.ac.kr>
2022-05-21 21:14:52 +0900

    add: 프로젝트 구조 세팅 (#1)

---
+++ ming/ming/Screen/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ming
+//
+//  Created by 김윤서 on 2022/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
